73a8d0f53715531d7c13bd455be28d82bb035e52
ElisonLima <limagoncalves@live.com>
2020-01-19 20:52:34 -0300

    Inicializador adicionado
    Inicializador adicionado

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
+import ccxt
+
+
+
+
+#Inicio do código
+
+if _name_ == __main__:#se o nome do código executado for igual a "main"(principal), este será executado
+  #código abaixo principal 
 
